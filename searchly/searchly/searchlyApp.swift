0f974b81cd4c0d92e084d8e73f0c0191d0eec746
cobsccompy4231p-007 <cobsccompy4231p-007@NIBM-LAB04-PC04s-iMac.local>
2024-10-27 12:00:25 +0530

    Init

---
+++ searchly/searchly/searchlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  searchlyApp.swift
+//  searchly
+//
+//  Created by cobsccompy4231p-007 on 2024-10-27.
+//
+
+import SwiftUI
+
+@main
+struct searchlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
